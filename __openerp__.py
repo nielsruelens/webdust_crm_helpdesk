34e1b09ce9f95cfecfda7a581e3679326f1aed9d
Niels Ruelens <niels.ruelens@clubit.be>
2014-03-04 17:51:19 +0100

    1st commit to git

---
+++ __openerp__.py
@@ -0,0 +1,27 @@
+{
+    'name': 'webdust_crm_helpdesk',
+    'version': '1.0',
+    'category': 'Sales',
+    'description': "Clarify helpdesk tree",
+    'author': 'Niels Ruelens',
+    'website': 'http://clubit.be',
+    'summary': "Clarify helpdesk tree ",
+    'sequence': 9,
+    'depends': [
+        'crm_helpdesk',
+    ],
+    'data': [
+        'view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'images': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
